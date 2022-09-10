3c1347c40037c47c2376159e3c8f6d4c393c2495
Hajime Nozaki <nozaki369@outlook.jp>
2022-09-10 13:00:26 +0000

    Section 3.2

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,30 @@
-module MyLib (someFunc) where
+html_ :: String -> String
+html_ = el "html" 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+makeHtml :: String -> String -> String
+makeHtml title body = html_ (head_ (title_ title) <> body_ body)
+
+myhtml :: String
+myhtml =
+    makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
