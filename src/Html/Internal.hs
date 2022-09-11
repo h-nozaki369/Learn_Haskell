eb76936adeb53621ace23ade3162a2a51470d1d8
Hajime Nozaki <nozaki369@outlook.jp>
2022-09-11 08:07:43 +0000

    Section 3.7

---
+++ src/Html/Internal.hs
@@ -0,0 +1,47 @@
+module Html.Internal where
+
+newtype Html = Html String
+newtype Structure = Structure String
+
+type Title = String
+
+html_ :: Title -> Structure -> Html
+html_ title (Structure body) =
+    Html (el "head" (el "title" (escape title)) <> el "body" body)
+
+p_ :: String -> Structure
+p_ = Structure . el "p" . escape
+
+h1_ :: String -> Structure
+h1_ = Structure . el "h1" . escape
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) =
+    Structure (a <> b)
+
+getStructureString :: Structure -> String
+getStructureString struct =
+    case struct of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+    case html of
+    Html str -> str
+
+escape :: String -> String
+escape =
+    let
+        escapeChar c =
+            case c of
+            '<' -> "&lt;"
+            '>' -> "&gt;"
+            '&' -> "&amp;"
+            '"' -> "&quot;"
+            '\'' -> "&#39;"
+            _   -> [c]
+    in concat . map escapeChar
